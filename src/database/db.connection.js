068ac251ddc7f857f15e58305167bdbc3e9d52d3
HeinnerXD <heinner45@gmail.com>
2019-01-31 15:16:55 -0500

    Update

--- src/database/db.connection.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose')
-const Keys = require('../keys')
-mongoose.connect(Keys.URI, { useNewUrlParser: true }).then(db => console.log('Db connected')).catch(err => console.log(err))
--- src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const TestController = require('../controllers/test.controller')
-const routes = require('express').Router()
-
-routes.get('/home', TestController.Test)
-routes.get('/products', TestController.Test)
-
-
-module.exports = routes
